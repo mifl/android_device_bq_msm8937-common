1a0dd7cf8bf12fd8a78492cc330d31340fa3bd14
Srinivas Reddy Julakanti <jsreddy@codeaurora.org>
2017-02-13 12:25:37 -0800

    msm8937: audio: Rename aanc_tuning_mixer.txt
    For tuning the Receiver Aided Stereo(RAS), speaker mixer controls
    for RAS test device have been added for 8998. This is required for
    offline RAS module tuning. Change mixer control file name
    from aanc_xxx.txt to audio_xxx.txt as this file not only used
    for aanc anymore.
    
    Change-Id: I1b937020793c7c0909e4e5265afc62224fde97e7

--- msm8937.mk
+++ msm8937.mk
@@ -74,12 +74,12 @@ PRODUCT_PACKAGES += \
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
-    hardware/qcom/audio-caf/msm8996/configs/msm8937/aanc_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/aanc_tuning_mixer.txt \
     hardware/qcom/audio-caf/msm8996/configs/msm8937/audio_effects.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.conf \
     hardware/qcom/audio-caf/msm8996/configs/msm8937/audio_output_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy.conf \
     hardware/qcom/audio-caf/msm8996/configs/msm8937/audio_platform_info_extcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_extcodec.xml \
     hardware/qcom/audio-caf/msm8996/configs/msm8937/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy.conf \
+    hardware/qcom/audio-caf/msm8996/configs/msm8937/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt \
     $(LOCAL_PATH)/audio/mixer_paths_qrd_sku1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_qrd_sku1.xml \
     hardware/qcom/audio-caf/msm8996/configs/msm8937/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
     hardware/qcom/audio-caf/msm8996/configs/msm8937/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
