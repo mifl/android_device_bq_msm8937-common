04a1d13263d41e81ea21a97909c47cc7609fda73
Ameya Thakur <ameyat@codeaurora.org>
2017-04-25 14:54:15 -0700

    msm8937_32: Update per_mgr path
    Peripheral manager now sits in the vendor directory/partition. Update
    the init script to reflect the same.
    
    Change-Id: I9353b0330d85980527abd1a719c0c92a7f9aac5b

--- config.fs
+++ config.fs
@@ -10,6 +10,18 @@ user: AID_BLUETOOTH
 group: AID_BLUETOOTH
 caps: BLOCK_SUSPEND
 
+[vendor/bin/pm-service]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
+[system/vendor/bin/pm-service]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
 [system/bin/pm-service]
 mode: 0755
 user: AID_SYSTEM
