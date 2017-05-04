ae63cbcdbdddfbb9047c7ed0d21cf6ebf3c507f5
Bharat Pawar <bpawar@codeaurora.org>
2017-05-04 11:39:51 +0530

    msm8937_32: Powerhint XML config
    Change adds XML config for powerhints to be
    used as target specific file for select use
    cases.
    
    Change-Id: I6aa0b53692e4f87d08df04dfec3636496bcbc766
    CRs-Fixed: 1102518

--- msm8937.mk
+++ msm8937.mk
@@ -210,6 +210,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     power.msm8937
 
+# Powerhint configuration file
+PRODUCT_COPY_FILES += \
+     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/powerhint.xml
+
 # Properties
 -include $(LOCAL_PATH)/vendor_prop.mk
 
