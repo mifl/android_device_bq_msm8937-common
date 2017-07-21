3575a9c9fd1d5a647640f015a0f117b888a5b4a7
Uma Mehta <umamehta@codeaurora.org>
2017-07-21 16:20:34 +0530

    media: Copy vanilla media_profile to vendor/etc
    Copy the vanilla version of media_profile.xml to vendor/etc
    to resolve boot up issues with pure AOSP system image.
    
    Change-Id: I9badc477d21918db4acf732a6c91a9326eac3163

--- msm8937.mk
+++ msm8937.mk
@@ -187,7 +187,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs_8937.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(LOCAL_PATH)/media/media_codecs_performance_8937.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
-    $(LOCAL_PATH)/media/media_profiles_8937.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml
+    $(LOCAL_PATH)/media/media_profiles_8937.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml \
+    $(LOCAL_PATH)/media/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
