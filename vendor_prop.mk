d4018dbb5c1ab33fcf9f0b43c1088c2932b5fc76
zljing <zljing@codeaurora.org>
2017-08-02 14:44:58 +0800

    boot: shorten wait time to optimize shutdown time
    Mountservice will check if there exist processes occupy files. Launch
    many 3rd apps and set in background will cause many processes occupy
    files, which will cause mountservice spend 10~20s to unmount(sometimes
     also unmount fail with 20s time out). If there is no process occupy
    files, mountservice only need about 500ms. So shorten wait time from
    20s to 500ms, which will save 10~20s for shutdown time, total shutdown
    time is in 3s.
    
    CRs-Fixed: 2038121
    
    Change-Id: I4da7bbf4c86d05add7f1b1e328f70796f3bb8ee8

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -80,7 +80,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.apm_sim_not_pwdn=1 \
     persist.vendor.radio.custom_ecc=1 \
     persist.vendor.radio.sib16_support=1 \
-    persist.vendor.radio.rat_on=disable
+    persist.vendor.radio.rat_on=disable \
+    sys.vendor.shutdown.waittime=500
 
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
