1b2c208a7ee62cead229d3f3b7c946164227a2d4
Mike Cailean <mcailean@codeaurora.org>
2017-08-16 16:42:45 -0700

    config.fs: Add xtwifi-client mapping
    Set the fs configuration for xtwifi-client
    binaries.
    
    Change-Id: I3fc60005f3bfaaff7d9dbf2ea48bf8638c426950

--- config.fs
+++ config.fs
@@ -91,6 +91,12 @@ user:  AID_GPS
 group: AID_GPS
 caps: NET_BIND_SERVICE
 
+[vendor/bin/xtwifi-client]
+mode: 0755
+user:  AID_GPS
+group: AID_GPS
+caps: NET_BIND_SERVICE
+
 [firmware/]
 mode: 0771
 user: AID_SYSTEM
