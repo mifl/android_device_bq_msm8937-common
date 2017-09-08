6d0fdf1ee129b3855bad6e39ce578d712d24c4f2
Sashidhar Ganiga <sganig@codeaurora.org>
2017-09-08 16:42:21 +0530

    msm8937: Add USB binderized hal service to PRODUCT_PACKAGES
    The Treble project introduced binderized hal in Android O.
    For USB part, its Binderized hal was not compiled, thus the usb_hal
    service which was implemented in the hal can not be loaded by
    UsbPortManager.
    
    Add android.hardware.usb@1.0-service to PRODUCT_PACKAGES.
    
    Change-Id: I32d7f4c86875a2a8b576a7e421594cc8819fb4ad

--- msm8937.mk
+++ msm8937.mk
@@ -316,6 +316,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
 # Vulkan
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level-0.xml \
