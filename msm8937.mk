927a49c1ea3cb5b0a01ee81c0463f59977b0e440
Udaya Bhaskara Reddy Mallavarapu <udaym@codeaurora.org>
2017-09-19 21:34:25 +0530

    msm8937: Add android.hardware.drm@1.0-service.widevine
    Add android.hardware.drm@1.0-service.widevine to PRODUCT_PACKAGES.
    android.hardware.drm@1.0-service.widevine is needed for
    WV13 complete use case.
    
    Change-Id: Id5d6a53bb5d5a8bc677b783b584454b782a42289

--- msm8937.mk
+++ msm8937.mk
@@ -146,7 +146,8 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.0-service.widevine
 
 # FM
 PRODUCT_PACKAGES += \
