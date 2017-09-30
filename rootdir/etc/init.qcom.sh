aa3324c5cd56410963d1e13425d500809042e141
Rashed Abdel-Tawab <rashed@linux.com>
2017-09-30 13:30:45 -0400

    msm8937: Update ramdisk for oreo
     * Move most binaries to vendor
     * Create new /data/vendor dirs
     * Update some paths from CAF
     * Move qcom_diag to oem_2901
     * Update config.fs
    
    Change-Id: I9db3ed347d1d98867aa0b55846eba0734c847619

--- rootdir/etc/init.qcom.sh
+++ rootdir/etc/init.qcom.sh
@@ -1,5 +1,6 @@
-#!/vendor/bin/sh
-# Copyright (c) 2009-2017, The Linux Foundation. All rights reserved.
+#! /vendor/bin/sh
+
+# Copyright (c) 2009-2016, The Linux Foundation. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
@@ -50,7 +51,7 @@ start_msm_irqbalance_8939()
 
 start_msm_irqbalance()
 {
-	if [ -f /system/vendor/bin/msm_irqbalance ]; then
+	if [ -f /vendor/bin/msm_irqbalance ]; then
 		start msm_irqbalance
 	fi
 }
