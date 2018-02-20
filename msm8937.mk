ccc0ad6325df34c7e3fc55486932f093ca0a0b22
dianlujitao <dianlujitao@lineageos.org>
2018-02-20 22:47:08 +0800

    msm8937: Build LiveDisplay HAL
    Change-Id: I4bfac85c64599ced45983a86d98cfd810a97db99

--- msm8937.mk
+++ msm8937.mk
@@ -257,6 +257,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.aw2013 
 
+# LiveDisplay native
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@1.0-service-sdm
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs_8937.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
