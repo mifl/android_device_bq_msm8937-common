3ecb38dcbe37631a3ee5eabb13da37d8c36cc289
mifl <mifl.pc@gmail.com>
2018-09-01 19:23:44 +0200

    msm8937-common: Create common blob list and extract scripts
    Change-Id: I3fb59d9b4d9845db209b722f5f56811f274b7503
    Signed-off-by: mifl <mifl.pc@gmail.com>

--- msm8937.mk
+++ msm8937.mk
@@ -16,6 +16,9 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
+# Get non-open-source specific aspects
+$(call inherit-product-if-exists, vendor/bq/msm8937-common/msm8937-common-vendor.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
