b00a27815e07be6f7ba50e78a9be4b08a59ee0fa
mifl <mifl.pc@gmail.com>
2018-09-01 20:17:23 +0200

    msm8937-common: commnonize msm_irqbalance
    The files are identical in chaozu and tenshi, it is not necessary to maintain these separately
    
    Change-Id: Id433cbdab8db2d8b63aa5a20ca45af883ebd8245
    Signed-off-by: mifl <mifl.pc@gmail.com>

--- msm8937.mk
+++ msm8937.mk
@@ -140,6 +140,10 @@ PRODUCT_PACKAGES += \
     ipacm \
     IPACM_cfg.xml
 
+# IRQ Balancer
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:system/vendor/etc/msm_irqbalance.conf
+
 # IRSC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
