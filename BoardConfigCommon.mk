a97eb32164799ae818a9697c8e0f4c3ae9626f77
mifl <mifl.pc@gmail.com>
2018-09-04 20:04:35 +0200

    msm8937: Add separated recovery fstab.
    Based on caf file: recovery_non-AB_non-split_variant.fstab
    The fstab has been sorted to facilitate the apply future commits
    
    Change-Id: I6ca8210e3ecd3a0499259dbec822f495ee1415e5

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -197,7 +197,7 @@ BOARD_USES_QCOM_HARDWARE := true
 BOARD_USES_QC_TIME_SERVICES := true
 
 # Recovery
-TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.qcom
+TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/recovery/recovery.fstab
 
 # RIL
 TARGET_RIL_VARIANT := caf
