663f09412eaae8572d940a539783afed4ed0c0be
Manaf Meethalavalappu Pallikunhi <manafm@codeaurora.org>
2018-12-19 16:30:20 +0530

    msm8937_32: Enable Thermal HAL by default
    Enable Thermal HAL for msm8937_32. It adds Thermal HAL
    to manifest.xml and thermal service to PRODUCT_PACKAGES.
    
    Change-Id: Ie3b9ff54832eaf91882df62eddb096e9647e939f

--- msm8937.mk
+++ msm8937.mk
@@ -342,6 +342,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
